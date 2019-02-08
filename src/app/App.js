6c12d52f850d5322b0b6130923770cc24ceb4d5d
oğuzhan önder <oguzhanonder@oguzhans-MacBook-Pro.local>
2019-02-08 16:46:12 +0300

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react';
+import './App.css';
+import {
+  Route,
+  withRouter,
+  Switch
+} from 'react-router-dom';
+
+import {API_BASE_URL} from '../constants';
+
+import PollList from '../poll/PollList';
+import NewPoll from '../poll/NewPoll';
+import Login from '../user/login/Login';
+import Signup from '../user/signup/Signup';
+import Profile from '../user/profile/Profile';
+import AppHeader from '../common/AppHeader';
+import NotFound from '../common/NotFound';
+import axios from "axios";
+import LoadingIndicator from '../common/LoadingIndicator';
+import PrivateRoute from '../common/PrivateRoute';
+
+import { Layout, notification } from 'antd';
+import Cookies from "js-cookie";
+const { Content } = Layout;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+      isAuthenticated: false,
+      isLoading: false
+    }
+    this.handleLogout = this.handleLogout.bind(this);
+    this.loadCurrentUser = this.loadCurrentUser.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+
+    notification.config({
+      placement: 'topRight',
+      top: 70,
+      duration: 3,
+    });    
+  }
+
+  loadCurrentUser() {
+    this.setState({
+      isLoading: true
+    });
+
+      if(!document.cookie.includes("JSESSIONID")) {
+          this.props.history.push("/login");
+          this.setState({
+              isAuthenticated: false,
+              isLoading: false
+          });
+      }else{
+          axios.get(API_BASE_URL+'/user/loginUser',{withCredentials:true})
+              .then(res => {
+                      this.setState({
+                          currentUser: res,
+                          isAuthenticated: true,
+                          isLoading: false
+                      });
+
+                  },
+                  (error) => {
+                      this.setState({
+                          isAuthenticated: false,
+                          currentUser: null,
+                          isLoading: false
+                      });
+
+                  });
+      }
+
+
+  }
+
+  componentWillMount(){
+      this.loadCurrentUser();
+  }
+
+  handleLogout(redirectTo="/login", notificationType="success", description="You're successfully logged out.") {
+
+      axios.post(API_BASE_URL + "/logout",null,{withCredentials:true})
+          .then((result) => {
+                  Cookies.remove("JSESSIONID");
+                  this.setState({
+                      currentUser: null,
+                      isAuthenticated: false,
+                      isLoading: false
+                  });
+              },
+              (error) => {
+                  console.log(error);
+              });
+
+
+    this.props.history.push(redirectTo);
+    
+    notification[notificationType]({
+      message: 'Polling App',
+      description: description,
+    });
+  }
+
+  handleLogin() {
+    notification.success({
+      message: 'Polling App',
+      description: "You're successfully logged in.",
+    });
+    this.loadCurrentUser();
+    this.props.history.push("/");
+  }
+
+  render() {
+    if(this.state.isLoading) {
+      return <LoadingIndicator />
+    }
+    return (
+        <Layout className="app-container">
+          <AppHeader isAuthenticated={this.state.isAuthenticated} 
+            currentUser={this.state.currentUser} 
+            onLogout={this.handleLogout} />
+
+          <Content className="app-content">
+            <div className="container">
+              <Switch>      
+                <Route exact path="/"
+                  render={(props) => <PollList isAuthenticated={this.state.isAuthenticated}
+                      currentUser={this.state.currentUser} handleLogout={this.handleLogout} {...props} />}>
+                </Route>
+                <Route path="/login" 
+                  render={(props) => <Login onLogin={this.handleLogin} {...props} />}></Route>
+                <Route path="/signup" component={Signup}></Route>
+                <Route path="/users/:username" 
+                  render={(props) => <Profile isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} {...props}  />}>
+                </Route>
+                <PrivateRoute authenticated={this.state.isAuthenticated} path="/poll/new" component={NewPoll} handleLogout={this.handleLogout}></PrivateRoute>
+                <Route component={NotFound}></Route>
+              </Switch>
+            </div>
+          </Content>
+        </Layout>
+    );
+  }
+}
+
+export default withRouter(App);
---
+++ src/user/login/Login.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import './Login.css';
+import { Link } from 'react-router-dom';
+import { API_BASE_URL } from '../../constants';
+import axios from "axios";
+import Cookies from 'universal-cookie';
+
+import { Form, Input, Button, Icon, notification } from 'antd';
+const FormItem = Form.Item;
+
+class Login extends Component {
+    render() {
+        const AntWrappedLoginForm = Form.create()(LoginForm)
+        return (
+            <div className="login-container">
+                <h1 className="page-title">Login</h1>
+                <div className="login-content">
+                    <AntWrappedLoginForm onLogin={this.props.onLogin} />
+                </div>
+            </div>
+        );
+    }
+}
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                const loginRequest = Object.assign({}, values);
+
+                var formData = new FormData();
+                formData.append('username', loginRequest.username);
+                formData.append('password', loginRequest.password);
+
+                axios.post(API_BASE_URL + "/login", formData,
+                ).then((result) => {
+                        const cookies = new Cookies();
+                        cookies.set(
+                            result.data.sessionId.split(";")[0].split("=")[0],result.data.sessionId.split(";")[0].split("=")[1]);
+                        this.props.onLogin();
+                    },
+                    (error) => {
+                        if(error.status === 401) {
+                            notification.error({
+                                message: 'Polling App',
+                                description: 'Your Username or Password is incorrect. Please try again!'
+                            });
+                        } else {
+                            notification.error({
+                                message: 'Polling App',
+                                description: error.message || 'Sorry! Something went wrong. Please try again!'
+                            });
+                        }
+                    });
+            }
+        });
+
+
+
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <FormItem>
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username or email!' }],
+                    })(
+                    <Input 
+                        prefix={<Icon type="user" />}
+                        size="large"
+                        name="username"
+                        placeholder="Username" />
+                    )}
+                </FormItem>
+                <FormItem>
+                {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],
+                })(
+                    <Input 
+                        prefix={<Icon type="lock" />}
+                        size="large"
+                        name="password" 
+                        type="password" 
+                        placeholder="Password"  />                        
+                )}
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" htmlType="submit" size="large" className="login-form-button">Login</Button>
+                    Or <Link to="/signup">register now!</Link>
+                </FormItem>
+            </Form>
+        );
+    }
+}
+
+
+export default Login;
